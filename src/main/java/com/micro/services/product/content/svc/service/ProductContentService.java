1de58f22a2e4adb7723fb6cc7c5d3678cb463079
daviddai <david.dai.personal@gmail.com>
2019-11-26 22:43:31 +1100

    removed old code

--- src/main/java/com/micro/services/product/content/svc/service/ProductContentService.java
+++ src/main/java/com/micro/services/product/content/svc/service/ProductContentService.java
@@ -2,11 +2,6 @@ package com.micro.services.product.content.svc.service;
 
 import com.micro.services.product.content.svc.event.EventSubscriber;
 import com.micro.services.product.content.svc.model.ProductApiModel;
-import org.springframework.amqp.core.ExchangeTypes;
-import org.springframework.amqp.rabbit.annotation.Exchange;
-import org.springframework.amqp.rabbit.annotation.Queue;
-import org.springframework.amqp.rabbit.annotation.QueueBinding;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
@@ -17,11 +12,6 @@ public class ProductContentService {
 
     private Set<ProductApiModel> productApiModels = new HashSet<>();
 
-//    @RabbitListener(bindings = @QueueBinding(
-//            value = @Queue(value = "product-content"),
-//            exchange = @Exchange(value = "supplierExchange", type = ExchangeTypes.TOPIC),
-//            key = {"supplier.createProduct"}
-//    ))
     @EventSubscriber
     public void addProduct(String productCode) {
         productApiModels.add(new ProductApiModel(productCode, ""));
